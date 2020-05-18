ed655fce0fb600efde67f7959ddc7916c3f5a34d
Océane Patiny <oceane@patiny.com>
2020-05-18 10:51:32 +0200

    feat: the chat works

--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
 'use strict';
 
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 const mqtt = require('mqtt');
 
-const server = 'mqtt://localhost:1883';
-const topic = 'chat/nodejs';
+const user1 = process.argv[2];
+const user2 = process.argv[3];
+const pubTopic = `chat/${process.argv[2]}`;
+const subTopic = `chat/${process.argv[3]}`;
+const broker = process.argv[4] || 'mqtt://localhost:1883';
 
-const client = mqtt.connect(server);
+// console.log(pubTopic, subTopic, broker);
 
-return 42;
+const client = mqtt.connect(broker);
+
+client.on('connect', () => {
+  client.subscribe(subTopic, (err) => {
+    if (err) {
+      rl.write(err);
+    }
+  });
+});
+
+rl.on('line', (input) => {
+  let line = input.toString();
+  if (line.length === 0) {
+    rl.question(`${user1}: `, (message) => {
+      client.publish(pubTopic, message);
+    });
+  }
+});
+
+client.on('message', function (topic, message) {
+  if (topic === subTopic) {
+    // message is Buffer
+    let answer = message.toString();
+    rl.write(`${user2}: ${answer}\n`);
+  }
+});
